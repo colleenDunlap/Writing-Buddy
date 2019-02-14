87ea6b4a3e41ccd77bfa60f09960fb5d24bad527
Dunlap, Colleen <colleen.dunlap@marquette.edu>
2019-02-14 15:27:59 -0600

    PDF reader objects.  Surprised this worked on the first time.

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import PyPDF2
+
+#pdf import code from https://www.geeksforgeeks.org/working-with-pdf-files-in-python/
+
+#file Object
+pdfFileObj = open('samplePDF.pdf', 'rb')
+#reader Object
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+print(pdfReader.numPages)
+#page Object
+pageObj = pdfReader.getPage(0)
+#extract Text from Page
+print(pageObj.extractText())
+pdfFileObj.close()
 print('Imported PyPDF2');
 print('Imported PyPDF2');
