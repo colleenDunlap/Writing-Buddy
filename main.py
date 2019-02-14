2a56dcf2df45d13541eaf8753e3a8213547e8912
Dunlap, Colleen <colleen.dunlap@marquette.edu>
2019-02-14 15:34:00 -0600

    Multi Page Implementation

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import PyPDF2
 #pdf import code from https://www.geeksforgeeks.org/working-with-pdf-files-in-python/
 
 #file Object
-pdfFileObj = open('samplePDF.pdf', 'rb')
+pdfFileObj = open('samplePDFMultiPage.pdf', 'rb')
 #reader Object
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 print(pdfReader.numPages)
+numPages = pdfReader.numPages
 #page Object
-pageObj = pdfReader.getPage(0)
+for iPage in range(0,numPages):
+	pageObj = pdfReader.getPage(iPage)
 #extract Text from Page
 print(pageObj.extractText())
 pdfFileObj.close()
-print('Imported PyPDF2');
-print('Imported PyPDF2');
