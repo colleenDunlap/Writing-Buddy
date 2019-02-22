81d8f48d625f333964d9fb29e97270ced6893962
Dunlap, Colleen <colleen.dunlap@marquette.edu>
2019-02-22 15:00:40 -0600

    Returned multipage functionality and more word filters

--- main.py
+++ main.py
@@ -9,18 +9,23 @@ pdfFileObj = open('samplePDFMultiPage.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 print(pdfReader.numPages)
 numPages = pdfReader.numPages
+words = []
 #page Object
 for iPage in range(0,numPages):
 	pageObj = pdfReader.getPage(iPage)
 	text = pageObj.extractText()
 	#text.remove('\n')
-	words = text.split(' ')
-	words = list(filter(lambda x: x!= '\n', words))
-	words = list(filter(lambda x: x!= '', words))
-	words = list(filter(lambda x: len(x)>3, words))
-	print(words)
+	words2 = text.split(' ')
+	words2 = list(filter(lambda x: x!= '\n', words2))
+	words2 = list(filter(lambda x: x!= '', words2))
+	words2 = list(filter(lambda x: len(x)>3, words2))
+	print(words2)
+	words.extend(words2)
 for iWord in range(0, len(words)):
 	words[iWord] = words[iWord].replace(",","")
+	words[iWord] = words[iWord].replace("/","")
+	words[iWord] = words[iWord].replace("(","")
+	words[iWord] = words[iWord].replace(")","")
 	words[iWord] = words[iWord].replace(".","")
 	words[iWord] = words[iWord].replace("-","")
 	words[iWord] = words[iWord].replace("\n","")
