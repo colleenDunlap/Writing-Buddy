358dc1ed66f544d391d8d6874e528d82a7ca2b14
Dunlap, Colleen <colleen.dunlap@marquette.edu>
2019-03-23 14:01:28 -0500

    Remove extra prints

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import binascii
 pdfFileObj = open('samplePDFMultiPage.pdf', 'rb')
 #reader Object
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-print(pdfReader.numPages)
 numPages = pdfReader.numPages
 words = []
 #page Object
@@ -19,7 +18,6 @@ for iPage in range(0,numPages):
 	words2 = list(filter(lambda x: x!= '\n', words2))
 	words2 = list(filter(lambda x: x!= '', words2))
 	words2 = list(filter(lambda x: len(x)>3, words2))
-	print(words2)
 	words.extend(words2)
 for iWord in range(0, len(words)):
 	words[iWord] = words[iWord].replace(",","")
@@ -30,7 +28,6 @@ for iWord in range(0, len(words)):
 	words[iWord] = words[iWord].replace("-","")
 	words[iWord] = words[iWord].replace("\n","")
 	words[iWord] = words[iWord].lower()
-	print(words[iWord])
 print(words)
 pdfFileObj.close()
 
