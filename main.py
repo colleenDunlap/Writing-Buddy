b3056372a18638147b654b608d32dae337fb34e3
Paulavancamp <paula.van.camp@gmail.com>
2019-03-27 22:49:45 -0500

    implemented defs and syns

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import PyPDF2
 import unicodedata
 import binascii
+from thesaurus import Word
+from PyDictionary import PyDictionary
+
 #pdf import code from https://www.geeksforgeeks.org/working-with-pdf-files-in-python/
 #array of object code inspiration https://www.daniweb.com/programming/software-development/code/216631/a-list-of-class-objects-python
 #linked list object code inspiration https://dbader.org/blog/python-linked-list
+
+#Function to calculate the ascii sum and return hash index using key of 89
 def calcHash(word):
 	wordSum = 0;
 	for iLetter in range(0, len(word)):
 		wordSum = wordSum + (ord(word[iLetter]))#return ascii
 	return (wordSum%89)
+
+#a node in a singly-linked list
 class ListNode:
-    """
-    A node in a singly-linked list.
-    """
     def __init__(self, frequency=None, next=None, word=None):
         self.frequency = frequency
         self.next = next
         self.word = word
     def __repr__(self):
         return repr(self.frequency)
+
 class SinglyLinkedList:
 	def __init__(self):
 		self.head = None
@@ -42,6 +47,7 @@ class SinglyLinkedList:
 			curr = curr.next
 		return '[' + ', '.join(nodes) + ']'
 
+
 #file Object
 pdfFileObj = open('samplePDFMultiPage.pdf', 'rb')
 #reader Object
@@ -51,6 +57,7 @@ words = []
 chainList = []
 for iList in range(0, 89):#because we mod by 89, only 89 possible hashes
 	chainList.append(SinglyLinkedList())
+
 #page Object
 for iPage in range(0,numPages):
 	pageObj = pdfReader.getPage(iPage)
@@ -72,10 +79,18 @@ for iWord in range(0, len(words)):
 	words[iWord] = words[iWord].lower()
 
 pdfFileObj.close()
+
+#loop to store each word in the hash table
 for iWord in range(0, len(words)):
 	hash = calcHash(words[iWord])
-	print(hash);
-	test = chainList[hash].append(words[iWord])
-	print(test.word);
-print(chainList[18])
+	chainList[hash].append(words[iWord])
+print(chainList[18]) #test to print an index with known collisions
+
+print('Synonyms:')
+input_word = Word('community') #chose "community" as a test word
+print(input_word.synonyms())
+dictionary=PyDictionary()
+print('Definition:')
+print (dictionary.meaning('community'))
+
 
