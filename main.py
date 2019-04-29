9d1075d077693c6e3b6f7d5b6889150c81d56435
Dunlap, Colleen <colleen.dunlap@marquette.edu>
2019-04-29 18:24:41 -0500

    I hate this

--- main.py
+++ main.py
@@ -47,14 +47,74 @@ class SinglyLinkedList:
 			curr = curr.next
 		return '[' + ', '.join(nodes) + ']'
 
+class DoubleListNode:
+	def __init__(self, frequency=None, next=None, word=None, prev = None):
+		self.frequency = frequency
+		self.next = next
+		self.prev = prev
+		self.word = word
+	def __repr__(self):
+		return repr(self.word)
 
+class DoublyLinkedList:
+	def __init__(self):
+		self.head = None
+		self.tail = None
+	def append(self, word):
+		if not self.head:
+			self.head = DoubleListNode(word=word, frequency = 0)
+			self.tail = self.head
+			self.head.next = None;
+			return self.head
+		curr = self.head
+		while curr.next:
+			curr = curr.next	
+		curr.next = DoubleListNode(word=word, frequency = 0)
+		curr.prev = curr;
+		return curr.next;
+	def insert(self, word, frequency):
+		curr = self.head;
+		while curr:
+			if(curr.word==word):
+				print("EQUIVALENT WORDS")
+				return;
+			curr = curr.next;
+		curr = self.head;
+		while curr.next:
+
+			if(curr.frequency<frequency):
+				n = DoubleListNode(word = word, frequency=frequency)
+				print(n.word);
+				if(curr == self.head):
+					self.head = n;
+				n.next = curr.next;
+				curr.next.prev = n;
+				n.prev = curr.prev;
+				curr.prev.next = n;
+			curr = curr.next;
+	def __repr__(self):
+		nodes = []
+		curr = self.head
+		while curr:
+			nodes.append(repr(curr))
+			curr = curr.next
+		return '[' + ', '.join(nodes) + ']'
 #file Object
 pdfFileObj = open('samplePDFMultiPage.pdf', 'rb')
 #reader Object
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 numPages = pdfReader.numPages
+
 words = []
 chainList = []
+
+cacheMin = 0;
+
+cacheList = []
+cacheList = DoublyLinkedList();
+for iList in range(0,20):#frequent words cache
+	cacheList.append("")
+print(cacheList);
 for iList in range(0, 89):#because we mod by 89, only 89 possible hashes
 	chainList.append(SinglyLinkedList())
 
@@ -66,7 +126,7 @@ for iPage in range(0,numPages):
 	words2 = text.split(' ')
 	words2 = list(filter(lambda x: x!= '\n', words2))
 	words2 = list(filter(lambda x: x!= '', words2))
-	words2 = list(filter(lambda x: len(x)>3, words2))
+	words2 = list(filter(lambda x: len(x)>5, words2))
 	words.extend(words2)
 for iWord in range(0, len(words)):
 	words[iWord] = words[iWord].replace(",","")
@@ -77,17 +137,22 @@ for iWord in range(0, len(words)):
 	words[iWord] = words[iWord].replace(")","")
 	words[iWord] = words[iWord].replace(".","")
 	words[iWord] = words[iWord].replace("-","")
+	words[iWord] = words[iWord].replace("that","")
 	words[iWord] = words[iWord].replace("\n","")
 	words[iWord] = words[iWord].lower()
 
 pdfFileObj.close()
-print(words);
+#print(words);
 #loop to store each word in the hash table
+print(cacheList);
 for iWord in range(0, len(words)):
 	hash = calcHash(words[iWord])
-	chainList[hash].append(words[iWord])
+	node = chainList[hash].append(words[iWord])
+	if (node.frequency>cacheMin):
+		cacheList.insert(node.word, node.frequency)
+		cacheMin = cacheList.tail.frequency;
 print(chainList[18]) #test to print an index with known collisions
-
+print(cacheList)
 print('Synonyms:')
 input_word = Word('community') #chose "community" as a test word
 print(input_word.synonyms())
